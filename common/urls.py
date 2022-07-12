fee55280ee7a8ed32092e8b04d908a1f55bc493e
yejikim00 <kimyeji2902@gmail.com>
2022-07-12 15:17:10 +0900

    post.html 연결, header.html, main_contet.html 분리

---
+++ common/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from common import views
+
+app_name = 'common'
+
+urlpatterns = [
+]
--- yj_blog/urls.py
+++ yj_blog/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import common.views
 import blog.views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', common.views.index, name='index'),
+    path('blog/', include('blog.urls')),
+    path('common/', include('common.urls')),
+    path('', blog.views.index, name='index'),
 ]
